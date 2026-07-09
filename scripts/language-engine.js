7dd89a72db87014a3376adf67187aa48237d23fc
gcotta-cell <gcotta@gmail.com>
2026-07-08 21:08:52 -0700

    Update language-engine.js

--- scripts/language-engine.js
+++ scripts/language-engine.js
@@ -25,13 +25,14 @@
       { selector: '[data-preview-item="products"] .section__heading, .products .section__heading', key: "classes.title" },
       { selector: '[data-preview-item="products"] .section__subheading, .products .section__subheading', key: "classes.subtitle" },
 
-      { selector: '[data-preview-item="text"] .section__heading, [data-preview-item="text-image"] .section__heading', key: "founder.title" },
-      { selector: '[data-preview-item="text"] p:nth-of-type(1), [data-preview-item="text-image"] p:nth-of-type(1)', key: "founder.welcome" },
-      { selector: '[data-preview-item="text"] p:nth-of-type(2), [data-preview-item="text-image"] p:nth-of-type(2)', key: "founder.paragraph_1" },
-      { selector: '[data-preview-item="text"] p:nth-of-type(3), [data-preview-item="text-image"] p:nth-of-type(3)', key: "founder.paragraph_2" },
-      { selector: '[data-preview-item="text"] p:nth-of-type(4), [data-preview-item="text-image"] p:nth-of-type(4)', key: "founder.paragraph_3" },
-      { selector: '[data-preview-item="text"] p:nth-of-type(5), [data-preview-item="text-image"] p:nth-of-type(5)', key: "founder.paragraph_4" },
-      { selector: '[data-preview-item="text"] p:nth-of-type(6), [data-preview-item="text-image"] p:nth-of-type(6)', key: "founder.paragraph_5" },
+      { selector: '[data-preview-item="text"] .section__heading, [data-preview-item="text-image"] .section__heading, .text-media .section__heading', key: "founder.title" },
+      
+      { selector: '[data-preview-item="text"] .section__body p:nth-child(1), [data-preview-item="text-image"] .section__body p:nth-child(1)', key: "founder.welcome" },
+      { selector: '[data-preview-item="text"] .section__body p:nth-child(2), [data-preview-item="text-image"] .section__body p:nth-child(2)', key: "founder.paragraph_1" },
+      { selector: '[data-preview-item="text"] .section__body p:nth-child(3), [data-preview-item="text-image"] .section__body p:nth-child(3)', key: "founder.paragraph_2" },
+      { selector: '[data-preview-item="text"] .section__body p:nth-child(4), [data-preview-item="text-image"] .section__body p:nth-child(4)', key: "founder.paragraph_3" },
+      { selector: '[data-preview-item="text"] .section__body p:nth-child(5), [data-preview-item="text-image"] .section__body p:nth-child(5)', key: "founder.paragraph_4" },
+      { selector: '[data-preview-item="text"] .section__body p:nth-child(6), [data-preview-item="text-image"] .section__body p:nth-child(6)', key: "founder.paragraph_5" },
 
       { selector: '[data-preview-item="showcase"] .section__heading', key: "social.title" }
     ],
